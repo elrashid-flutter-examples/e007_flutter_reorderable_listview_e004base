140186ec923561a3b1ca88727cec7049d022a60f
moh <osman.elrashid@gmail.com>
2019-07-27 22:19:52 +0400

    initial files

---
+++ android/app/src/main/java/com/example/e007_flutter_reorderable_listview_e004base/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.e007_flutter_reorderable_listview_e004base;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
